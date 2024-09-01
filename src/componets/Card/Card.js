931a71e4f3abd2524607b972f7699ef275f58ba7
naoya-fukushima <naoya.sio40@gmail.com>
2024-09-01 20:22:01 +0900

    ポケモンデータをAPIから取得し、詳細情報を表示する機能を作成

---
+++ src/componets/Card/Card.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export const Card = ({ pokemon }) => {
+  return (
+    <div className="card">
+      <div className="cardImg">
+        <img src={pokemon.sprites.front_default} />
+      </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardTypes">
+        <div>タイプ</div>
+        {pokemon.types.map((type) => {
+          return (
+            <div>
+              <span className="typeName">{type.type.name}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">重さ:{pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">高さ:{pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">
+            アビリティ:
+            {pokemon.abilities.map((ability) => {
+              return (
+                <div>
+                  <span className="abilityName">{ability.ability.name}</span>
+                </div>
+              );
+            })}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
